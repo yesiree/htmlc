df9c2b602443193e15a43e25fcbad89fe2e968c9
Trevor <trevorhreed@gmail.com>
2022-12-07 00:36:28 -0700

    support images

--- test/src/code.js
+++ test/src/code.js
@@ -1,3 +1,3 @@
 document.addEventListener('DOMContentLoaded', () => {
-  document.querySelector('h1').textContent += ' (JavaScript is working!)'
+  document.querySelector('#javascript').textContent += 'JavaScript is working!'
 })
