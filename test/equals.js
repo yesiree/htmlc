f0f16cb747487aa473f513fc4713640e280f846e
Trevor Reed <trevor.reed@familysearch.org>
2024-12-01 00:39:47 -0700

    adding new features

--- test/equals.js
+++ test/equals.js
@@ -1,4 +1,7 @@
 import fs from 'fs/promises'
+import colors from 'colors'
+import * as Diff from 'diff'
+
 
 const [actual, expected] =
   await Promise.all(
@@ -8,8 +11,36 @@ const [actual, expected] =
   )
 
 if (actual.equals(expected)) {
-  console.log('Passed.')
+  console.log('  ✅ Passed.\n')
 } else {
-  console.log(`Failed:\n\nActual:\n=========\n${actual}\n=========\n\nExpected:\n=========\n${expected}\n=========\n`)
+  console.log(`  ❌ Failed.`)
+
+  const actualStr = actual.toString()
+  const expectedStr = expected.toString()
+  const minLength = Math.min(actualStr.length, expectedStr.length)
+  let index = 0
+  while (index < minLength && actualStr[index] === expectedStr[index]) index++
+  if (index > 0 && index < minLength) {
+    const snip = whole => whole.slice(index - 20, index + 20)
+      .split('\n')
+      .map(x => `      |${x}|`)
+      .join('\n')
+
+    const actualSnippet = snip(actualStr)
+    const expectedSnippet = snip(expectedStr)
+
+      console.log(`  Diff found at index ${index}:\n\n    Actual:\n\n${actualSnippet}\n\n    Expected:\n\n${expectedSnippet}|\n`)
+  }
+
+  // const diff = Diff.diffChars(actual.toString(), expected.toString())
+  // diff.forEach((part) => {
+  //   const text = part.added
+  //     ? colors.green(part.value)
+  //     : part.removed
+  //       ? colors.red(part.value)
+  //       : part.value
+  //   process.stderr.write(text.slice(0, 1000))
+  // })
+
   process.exit(1)
 }
